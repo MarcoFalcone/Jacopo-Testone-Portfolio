b32f494fbe0f0a03ae6a6b77ee08c3157cb3db0d
Markrius <marco.falcone@hotmail.it>
2021-01-17 01:27:19 +0100

    work in progress

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import './Contact.css';
 
 const Contact = (props) => (
   <div class="socials">
-    <a class="mail" href="mailto:mfalcone.dev@gmail.com">jacopotestone@gmail.com</a>
+    <a class="mail" href="mailto:jacopotestone@gmail.com">jacopotestone@gmail.com</a>
     <a href="https://www.instagram.com/jacopotestone/" target='_blank'><img class="insta" src="https://ik.imagekit.io/7ol7k87qcca/Boia/insta_tPGHuiq7Z.png" /></a>
     <div className="closecontact" onClick={props.close} />
   </div>
