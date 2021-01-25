fa62ad1f2c6d49772dc3294b8fd6462011035a9d
Markrius <marco.falcone@hotmail.it>
2021-01-25 17:44:01 +0100

    work in progress

--- src/components/background.js
+++ src/components/background.js
@@ -3,7 +3,7 @@ import '../styles/background.css';
 const Background = () => (
     <div className='wrap1'>
       <video className="background" src="https://ik.imagekit.io/7ol7k87qcca/Boia/background_mJA2Y46sAH7as.m4v" type="video/mp4" autoPlay loop muted playsInline ></video>
-      <video className="backgrounddevice" src="https://ik.imagekit.io/7ol7k87qcca/Boia/test_pJhGvORHpid5A.mp4" type="video/mp4" autoPlay loop muted playsInline ></video>
+      <video className="backgrounddevice" src="https://ik.imagekit.io/7ol7k87qcca/Boia/test2_Jn1t11jPW.m4v" type="video/mp4" autoPlay loop muted playsInline ></video>
       <div className='shroud'></div>
     </div>
   );
