5d23b436bfcb155165fe8fc0977ae5ffd3de52c6
Markrius <marco.falcone@hotmail.it>
2021-02-11 20:00:43 +0100

    update translations

--- src/translation.js
+++ src/translation.js
@@ -15,11 +15,12 @@ export const resources = {
 
       card3 : "SHE DOESN'T KNOW HOW TO LOVE - Diego is an inattentive boy who delivers pizzas for his uncle's pizzeria. The casual-awkward meeting of a prostitute from East Europe makes Diego more and more curious about the girl's character. He will try to meet her again until he decides to help the girl to live a different life.",
 
-      card4 : "This is a resume of some music videos I shot between 2017 and 2019; during these years I shot more than 30 music videos from independent artists. All the videos were shot, directed and edited by myself under the pseudonym of Test1Prod.",
+      card4 : "TEST1PROD videoclip showreel - This is a resume of some music videos I shot between 2017 and 2019; during these years I shot more than 30 music videos from independent artists. All the videos were shot, directed and edited by myself under the pseudonym of Test1Prod.",
 
       title : "WATCH TRAILER"
     }
   },
+
   it: {
     translation: {
       about : "CHI SONO",
@@ -28,15 +29,15 @@ export const resources = {
 
       works : "LAVORI",
 
-      txt : "I film sono sempre stati la mia passione, il mio scopo. Ho passato i miei 20 anni viaggiando, studiando all'estero e guardando film. Poi ho iniziato a lavorare con il cinema, scrivere film e dirigere video. Ora che ho appena iniziato gli anni Trenta sono pronto per girare il mio primo lungometraggio. Nel frattempo, sono fiducioso di lavorare per società di produzione per mantenere i contatti nell'industria cinematografica.",
+      txt : "Il cinema è sempre stato la mia passione. Ho trascorso i miei vent'anni viaggiando, ho studiato all'estero e guardato in ogni caso molti film. Poi ho iniziato a lavorare per varie produzioni, ho scritto e girato alcuni cortometraggi e alcuni videoclip per trovare uno stile, una tecnica. Ora che ho appena varcato la soglia dei trent'anni mi sento pronto per girare il primo lungometraggio; nel frattempo mi accontento di continuare a lavorare per le case di produzione per accrescere i miei contatti nell'industria cinematografica.",
 
-      card1 : "LO STRUMENTO DELLA FAMA - Ludovico Nenni è un attore trentenne che vive nello stesso teatro in cui lavora. Dato che è stanco di una situazione così instabile, accetta di fare un'audizione particolare in un luogo nascosto con persone eccentriche. L'audizione consiste in un “test di fama” in cui il candidato deve proiettarsi su una realtà virtuale e affrontare lo stress di essere una superstar. L'audizione lascerà a Ludovico un segno particolare anche a distanza di diversi mesi dalla sua fine, tanto che dovrà decidere quali valori deve abbandonare per diventare quello che vuole essere: un vero attore o solo una superstar?",
+      card1 : "LO STRUMENTO DELLA FAMA - Ludovico Nenni è un attore trentenne che vive nello stesso teatro in cui lavora. Stanco di una situazione così instabile, accetta di fare un provino particolare in un luogo nascosto con persone eccentriche. Il provino consiste in un “test di fama” in cui il candidato deve proiettarsi su una realtà virtuale e affrontare lo stress di essere una superstar. Il provino lascerà a Ludovico un segno particolare anche a distanza di diversi mesi dalla sua fine, tanto che dovrà decidere quali valori deve abbandonare per diventare quello che vuole essere: un vero attore o solo una superstar?",
 
-      card2 : "POVERO E INFAME - Questa è la storia di un uomo che vive come un ribelle contro la società, portando avanti un piano visionario di fuga dalla città in cui vive. Ha costantemente in testa il mito di Jim Morrison, un personaggio che cerca di seguire il suo stile di vita, basato su una perdizione totale.",
+      card2 : "POVERI E INFAMI - Questa è la storia di Jimmy, un uomo che vive come un ribelle e rifiuta ogni valore che la società gli offre. Vive col mito di Jim Morrison, personaggio del quale cerca di seguire il suo stile di vita basato su una perdizione totale.",
 
-      card3 : "PERCHE' NON SA VOLER BENE - Diego è un ragazzo disattento che consegna pizze per la pizzeria di suo zio. L'incontro casuale-imbarazzante di una prostituta dell'Europa orientale rende Diego sempre più curioso del carattere della ragazza. Proverà a incontrarla di nuovo fino a quando non deciderà di aiutare la ragazza a vivere una vita diversa.",
+      card3 : "PERCHE' NON SA VOLER BENE - Diego è un ragazzo distratto che fa il fattorino per la pizzeria di suo zio. L’incontro casuale-imbarazzante con una prostituta dell’est rende Diego sempre più curioso del mistero che avvolge la ragazza. Proverà a incontrarla di nuovo fino a quando non deciderà di aiutare la ragazza a vivere una vita migliore.",
 
-      card4 : "Questo è un riassunto di alcuni video musicali che ho girato tra il 2017 e il 2019; in questi anni ho girato più di 30 video musicali di artisti indipendenti. Tutti i video sono stati girati, diretti e modificati da me con lo pseudonimo di Test1Prod.",
+      card4 : "TEST1PROD videoclip showreel - Un mix di videoclip che ho girato tra il 2017 e il 2019; ho girato in totale più di 30 videoclip di vari artisti indipendenti e auto prodotti. Tutti i video sono stati ideati, girato e montati con lo pseudonimo di Test1Prod.",
 
       title : "GUARDA TRAILER"
     }
